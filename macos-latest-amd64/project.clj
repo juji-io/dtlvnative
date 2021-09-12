b5b2f2bd570e15df69d6fcde2e2c070c0c9425e1
Huahai Yang <huahai.yang@gmail.com>
2021-09-11 22:28:09 -0700

    buld

--- macos-latest-amd64/project.clj
+++ macos-latest-amd64/project.clj
@@ -1,9 +1,10 @@
-(defproject dtlvnative-macos-amd64 "0.1.0"
+(defproject dtlvnative-macos-latest-amd64 "0.1.0"
   :description "Native dependency of Datalevin database"
   :url "https://github.com/juji-io/dtlvnative"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
+  :resource-paths ["../resources"]
   :deploy-repositories [["clojars" {:url           "https://repo.clojars.org"
                                     :username      :env/clojars_username
                                     :password      :env/clojars_password
