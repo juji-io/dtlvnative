e9087812cf660dfd87fbe20000be9f147c80232f
Huahai Yang <huahai.yang@gmail.com>
2023-01-29 18:58:56 -0800

    Version 0.8.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Change Log
 
-## WIP
+## 0.8.2
 ### Fixed
 - Windows build
 
--- windows-amd64/project.clj
+++ windows-amd64/project.clj
@@ -1,4 +1,4 @@
-(def version "0.8.1")
+(def version "0.8.2")
 
 (defproject org.clojars.huahaiy/dtlvnative-windows-amd64 version
   :description "Native dependency of Datalevin database"
