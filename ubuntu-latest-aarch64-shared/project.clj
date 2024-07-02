874054d5cb00d31a6e2f8600f54c09c1e72cd772
Aleksandr Bogdanov <sasha_bogdanov_dev@yahoo.com>
2024-07-02 17:39:51 +0500

    organization

--- ubuntu-latest-aarch64-shared/project.clj
+++ ubuntu-latest-aarch64-shared/project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.algoflora/dtlvnative-linux-aarch64-shared "0.9.4-SNAPSHOT"
+(defproject org.clojars.huahaiy/dtlvnative-linux-aarch64-shared "0.9.4-SNAPSHOT"
   :description "Native dependency of Datalevin database"
   :url "https://github.com/juji-io/dtlvnative"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
