57bc55e3de01f9c36fe9c93709c832b34bba4168
Aleksandr Bogdanov <sasha_bogdanov_dev@yahoo.com>
2024-07-02 15:08:59 +0500

    Squashed commits

---
+++ ubuntu-latest-aarch64/project.clj
@@ -0,0 +1,10 @@
+(defproject io.github.algoflora/dtlvnative-linux-aarch64 "0.9.4-SNAPSHOT"
+  :description "Native dependency of Datalevin database"
+  :url "https://github.com/juji-io/dtlvnative"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url  "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.11.2"]]
+  :deploy-repositories [["clojars" {:url           "https://repo.clojars.org"
+                                    :username      :env/clojars_username
+                                    :password      :env/clojars_password
+                                    :sign-releases false}]])
