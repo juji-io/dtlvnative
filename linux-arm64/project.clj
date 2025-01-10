59fa009241f502c6842048498aae535965045b9f
Huahai Yang <hyang@juji-inc.com>
2025-01-10 12:19:54 -0800

    linux-arm64

---
+++ linux-arm64/project.clj
@@ -0,0 +1,14 @@
+(defproject org.clojars.huahaiy/dtlvnative-linux-arm64 "0.11.0"
+  :description "Native dependency of embedded library version of Datalevin on Linux Arm64"
+  :url "https://github.com/juji-io/dtlvnative"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url  "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.12.0"]
+                 [org.bytedeco/javacpp "1.5.11"]]
+  :java-source-paths ["../src/java"]
+  :javac-options ["-Xlint:unchecked" "-Xlint:-options" "--release" "8"]
+  :main datalevin.dtlvnative.Test
+  :deploy-repositories [["clojars" {:url           "https://repo.clojars.org"
+                                    :username      :env/clojars_username
+                                    :password      :env/clojars_password
+                                    :sign-releases false}]])
