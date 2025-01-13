ead734ae609814e1fd8af71c3f62af6f38e15299
Huahai Yang <hyang@juji-inc.com>
2025-01-12 17:35:24 -0800

    javacpp wants static lib on windows

--- script/build.bat
+++ script/build.bat
@@ -1,11 +1,6 @@
 
 set PWD=%cd%
 set CPATH=%PWD%\src
-set DPATH=%PWD%\windows-x86_64\resources\datalevin\dtlvnative\windows-x86_64
-
-mkdir %DPATH% /p
-
-mkdir %CPATH%\java\datalevin\dtlvnative\windows-x86_64 /p
 
 cd %CPATH%
 
@@ -16,12 +11,10 @@ cd build
 cmake .. ^
     -G "NMake Makefiles" ^
     -DCMAKE_BUILD_TYPE:STRING=RELEASE ^
-    -DCMAKE_INSTALL_PREFIX=%DPATH% ^
+    -DCMAKE_INSTALL_PREFIX=%CPATH% ^
     -DCLOSE_WARNING=on ^
     -DBUILD_TEST=off
 
 nmake install
 
-dir %DPATH%
-
-cd %PWD%
+dir %CPATH%
