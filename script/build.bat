9b01e07b0c39ad7811ba8a06ae49a6c264881763
Huahai Yang <huahai.yang@gmail.com>
2025-01-29 13:19:19 -0800

    try debug build

--- script/build.bat
+++ script/build.bat
@@ -5,25 +5,24 @@ set CPATH=%PWD%\src
 
 cd %CPATH%\usearch
 
-cmake ^
-    -D CMAKE_BUILD_TYPE=Release ^
-    -D USEARCH_USE_FP16LIB=1 ^
-    -D USEARCH_USE_OPENMP=1 ^
-    -D USEARCH_USE_SIMSIMD=1 ^
-    -D USEARCH_BUILD_TEST_CPP=1 ^
-    -D USEARCH_BUILD_TEST_C=1 ^
-    -D USEARCH_BUILD_LIB_C=1 ^
-    -B build_release
+cmake -G "Visual Studio 17 2022" ^
+      -D USEARCH_USE_FP16LIB=1 ^
+      -D USEARCH_USE_OPENMP=1 ^
+      -D USEARCH_USE_SIMSIMD=1 ^
+      -D USEARCH_BUILD_TEST_CPP=1 ^
+      -D USEARCH_BUILD_TEST_C=1 ^
+      -D USEARCH_BUILD_LIB_C=1 ^
+      -B build_us
 
-cmake --build build_release --config Release
+cmake --build build_us --config Debug
 
-build_release\test_cpp
+build_us\test_cpp
 
-build_release\test_c
+build_us\test_c
 
-dir build_release
+dir build_us
 
-copy build_release\libusearch_static_c.lib %CPATH%\usearch.lib
+copy build_us\libusearch_static_c.lib %CPATH%\usearch.lib
 
 cd %PWD%
 
