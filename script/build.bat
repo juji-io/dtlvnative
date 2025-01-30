1585bd3e9209bb6a56708948d5fdb3037bce20ac
Huahai Yang <huahai.yang@gmail.com>
2025-01-30 07:48:25 -0800

    build usearch as subdir

--- script/build.bat
+++ script/build.bat
@@ -3,34 +3,11 @@ set PWD=%cd%
 
 set CPATH=%PWD%\src
 
-cd %CPATH%\usearch
-
-cmake -G "Visual Studio 17 2022" ^
-      -D CMAKE_BUILD_TYPE=Release ^
-      -D USEARCH_USE_FP16LIB=1 ^
-      -D USEARCH_USE_OPENMP=1 ^
-      -D USEARCH_USE_SIMSIMD=1 ^
-      -D USEARCH_BUILD_TEST_CPP=1 ^
-      -D USEARCH_BUILD_TEST_C=1 ^
-      -D USEARCH_BUILD_LIB_C=1 ^
-      -B build_us
-
-cmake --build build_us --config Release
-
-build_us\test_cpp
-
-build_us\test_c
-
-dir build_us
-
-copy build_us\libusearch_static_c.lib %CPATH%\usearch.lib
-
 cd %PWD%
 
 cd %CPATH%
 
 cmake -G "Visual Studio 17 2022" ^
-      -D CMAKE_BUILD_TYPE=Release ^
       -DCLOSE_WARNING=on ^
       -DBUILD_TEST=off ^
       -DCMAKE_INSTALL_PREFIX=%CPATH% ^
