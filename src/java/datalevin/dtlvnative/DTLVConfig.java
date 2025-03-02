1b8a19a73cbec150906abddbfc72437491ca8d52
Huahai Yang <huahai.yang@gmail.com>
2025-03-02 09:56:01 -0800

    try usearch as an independent lib

--- src/java/datalevin/dtlvnative/DTLVConfig.java
+++ src/java/datalevin/dtlvnative/DTLVConfig.java
@@ -16,7 +16,7 @@ import org.bytedeco.javacpp.tools.*;
                            ),
                 @Platform( // Windows
                            value = "windows",
-                           link = { "lmdb", "libusearch_static_c", "dtlv", "Advapi32" }
+                           link = { "lmdb", "usearch_static_c", "dtlv", "Advapi32" }
                            ),
                 @Platform( // Unix-like
                            value = { "linux", "macosx" },
