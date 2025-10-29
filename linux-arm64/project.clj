d140e5cc3d42b4408660afb81cf21c615481d5a8
Huahai Yang <huahai.yang@gmail.com>
2025-10-28 19:08:46 -0700

    Version 0.13.10

--- linux-arm64/project.clj
+++ linux-arm64/project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.huahaiy/dtlvnative-linux-arm64 "0.13.9"
+(defproject org.clojars.huahaiy/dtlvnative-linux-arm64 "0.13.10"
   :description "Native dependency of Datalevin on Linux Arm64"
   :url "https://github.com/juji-io/dtlvnative"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
