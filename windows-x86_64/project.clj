d8e04a9e9b7e994dae1b3213f2e0ba4967f7a416
Huahai Yang <huahai.yang@gmail.com>
2025-12-04 11:47:16 -0800

    Version 0.14.2

--- windows-x86_64/project.clj
+++ windows-x86_64/project.clj
@@ -1,4 +1,4 @@
-(def version "0.14.1")
+(def version "0.14.2")
 
 (defproject org.clojars.huahaiy/dtlvnative-windows-x86_64 version
   :description "Native dependency of Datalevin database on Windows"
