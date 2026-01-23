b559b98e5f6caf24a406db0d9cc7bc48ca233577
Huahai Yang <huahai.yang@gmail.com>
2026-01-23 14:21:08 -0800

    Version 0.15.8

--- macosx-arm64/project.clj
+++ macosx-arm64/project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.huahaiy/dtlvnative-macosx-arm64 "0.15.7"
+(defproject org.clojars.huahaiy/dtlvnative-macosx-arm64 "0.15.8"
   :description "Native dependency of Datalevin on Apple Silicon"
   :url "https://github.com/juji-io/dtlvnative"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
