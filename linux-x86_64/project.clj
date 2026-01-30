c759bec84a8c6db01132474f240e7f61c73d2280
Huahai Yang <huahai.yang@gmail.com>
2026-01-29 17:01:56 -0800

    Version 0.15.9

--- linux-x86_64/project.clj
+++ linux-x86_64/project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.huahaiy/dtlvnative-linux-x86_64 "0.15.8"
+(defproject org.clojars.huahaiy/dtlvnative-linux-x86_64 "0.15.9"
   :description "Native dependency of Datalevin on Linux x86-64"
   :url "https://github.com/juji-io/dtlvnative"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
