34a546d865f5c9c37f2df6583fb7f7579241d530
Huahai Yang <huahai.yang@gmail.com>
2026-03-10 08:05:26 -0700

    run one build at a time

--- script/build.bat
+++ script/build.bat
@@ -3,6 +3,8 @@ set PWD=%cd%
 
 set CPATH=%PWD%\src
 
+del /q "%CPATH%\dtlv.lib" "%CPATH%\llama.lib" "%CPATH%\ggml.lib" "%CPATH%\ggml-base.lib" "%CPATH%\ggml-cpu.lib" "%CPATH%\dlmdb.lib" "%CPATH%\libusearch_static_c.lib" 2>nul
+
 cd %PWD%
 
 cd %CPATH%
@@ -12,14 +14,33 @@ cmake -G "Visual Studio 17 2022" ^
       -DBUILD_TEST=off ^
       -DCMAKE_INSTALL_PREFIX=%CPATH% ^
       -B build_dtlv
+if errorlevel 1 exit /b %errorlevel%
 
-cmake --build build_dtlv --config Release --target install
+call :build_and_stage dlmdb dlmdb.lib
+if errorlevel 1 exit /b %errorlevel%
+call :build_and_stage usearch_static_c libusearch_static_c.lib
+if errorlevel 1 exit /b %errorlevel%
+call :build_and_stage ggml-base ggml-base.lib
+if errorlevel 1 exit /b %errorlevel%
+call :build_and_stage ggml-cpu ggml-cpu.lib
+if errorlevel 1 exit /b %errorlevel%
+call :build_and_stage ggml ggml.lib
+if errorlevel 1 exit /b %errorlevel%
+call :build_and_stage llama llama.lib
+if errorlevel 1 exit /b %errorlevel%
+call :build_and_stage dtlv dtlv.lib
+if errorlevel 1 exit /b %errorlevel%
+
+cmake --build build_dtlv --config Release --target test_cpp test_c
+if errorlevel 1 exit /b %errorlevel%
 
 dir build_dtlv
 
-build_dtlv\test_cpp
+call :run_built_exe test_cpp.exe
+if errorlevel 1 exit /b %errorlevel%
 
-build_dtlv\test_c
+call :run_built_exe test_c.exe
+if errorlevel 1 exit /b %errorlevel%
 
 cd %PWD%
 
@@ -41,15 +62,48 @@ cd java
 
 java -jar "%USERPROFILE%\.m2\repository\org\bytedeco\javacpp\1.5.13\javacpp-1.5.13.jar" ^
     datalevin/dtlvnative/DTLV.java
+if errorlevel 1 exit /b %errorlevel%
 
 dir datalevin\dtlvnative\windows-x86_64
 
-dumpbin /linkermember:2 datalevin\dtlvnative\windows-x86_64\jniDTLV.dll
+dumpbin /dependents datalevin\dtlvnative\windows-x86_64\jniDTLV.dll
+if errorlevel 1 exit /b %errorlevel%
 
 cd ..\..
 
 copy src\java\datalevin\dtlvnative\windows-x86_64\* windows-x86_64\resources\datalevin\dtlvnative\windows-x86_64\
+if errorlevel 1 exit /b %errorlevel%
 
 copy src\*.lib windows-x86_64\resources\datalevin\dtlvnative\windows-x86_64\
+if errorlevel 1 exit /b %errorlevel%
 
 dir windows-x86_64\resources\datalevin\dtlvnative\windows-x86_64
+
+goto :eof
+
+:build_and_stage
+echo Building %~1 and staging %~2...
+cmake --build build_dtlv --config Release --target %~1
+if errorlevel 1 exit /b %errorlevel%
+if exist "%CPATH%\%~2" goto :eof
+for /r build_dtlv %%F in (%~2) do copy /Y "%%F" "%CPATH%\%~2" >nul
+if exist "%CPATH%\%~2" goto :eof
+echo Missing %~2 after building %~1.
+dir %CPATH%\*.lib
+dir /s build_dtlv\*%~1*.lib
+exit /b 1
+
+:run_built_exe
+if exist "build_dtlv\Release\%~1" goto run_built_exe_release
+if exist "build_dtlv\%~1" goto run_built_exe_root
+echo Missing %~1 after build.
+dir /s build_dtlv\%~1
+exit /b 1
+
+:run_built_exe_release
+"build_dtlv\Release\%~1"
+exit /b %errorlevel%
+
+:run_built_exe_root
+"build_dtlv\%~1"
+exit /b %errorlevel%
